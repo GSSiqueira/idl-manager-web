6001b25d067e2feeb1888e253ef6b18c148ed540
Gabriel Siqueira <gssiqueira92@gmail.com>
2025-10-16 10:29:04 -0300

    start burger menu and new page

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 let acc = document.getElementsByClassName("accordion");
 let i;
+let burgerIcon = document.getElementById("burgerIcon");
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", toggleDropdown);
 }
+
+burgerIcon.addEventListener("click", function (){
+    this.classList.toggle("change");
+    this.classList.toggle("active");
+    let navBar = document.getElementById("navBar");
+    if (navBar.style.maxHeight) {
+        navBar.style.maxHeight = null;
+    } else {
+        navBar.style.maxHeight = navBar.scrollHeight + "px";/*Gets the element height in pixels to add and create the display effect*/
+    }
+});
 
 function toggleDropdown() {
     this.classList.toggle("active");
