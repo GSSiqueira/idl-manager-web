58bd8236309b3f23ac509fef06e8514c0b5c270d
Gabriel Siqueira <gssiqueira92@gmail.com>
2025-10-28 11:41:59 -0300

    add js so date default to today

---
+++ js/fluxo.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+        const dateInput = document.querySelector("#data");
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, '0');
+        const day = String(today.getDate()).padStart(2, '0');
+        const formattedDate = `${year}-${month}-${day}`;
+        dateInput.value = formattedDate;
+    });
